a3aa6c8aef25861e92a4cb44ddc7b0359befd257
Leonardo Rocca <lrocca@student.42roma.it>
2022-03-09 15:46:40 +0100

    ex02: more tests

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -5,7 +5,19 @@
 
 #define MAX_VAL 750
 
-void	checkValues(Array<int> y, int *x)
+void	checkValues(Array<int> x, int *y)
+{
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		if (x[i] != y[i])
+		{
+			std::cerr << "didn't save the same value!!" << std::endl;
+			exit(EXIT_FAILURE);
+		}
+	}
+}
+
+void	checkValues(Array<int> x, Array<int> y)
 {
 	for (int i = 0; i < MAX_VAL; i++)
 	{
@@ -32,10 +44,37 @@ int main(void)
 		mirror[i] = value;
 	}
 
+	// empty
+	try
+	{
+		Array<int>	empty_array;
+		empty_array[0] = 0;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	// invalid size
+	try
+	{
+		Array<int>	invalid[0];
+		// Array<int>	invalid(0);
+		invalid[0] = 0; // would have segfaulted
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
 	// copy
 	{
 		Array<int> tmp = numbers;
 		Array<int> test(tmp);
+
+		checkValues(test, tmp);
+		checkValues(numbers, tmp);
+		checkValues(numbers, test);
 
 		for (int i = 0; i < MAX_VAL; i++)
 		{
@@ -62,6 +101,8 @@ int main(void)
 	{
 		std::cerr << e.what() << std::endl;
 	}
+
+	std::cout << numbers[MAX_VAL - 1] << std::endl;
 
 	delete [] mirror;
 }
